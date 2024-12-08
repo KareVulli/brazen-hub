9ae9ceac7afc6c5d423d522c7b55483f359a09ea
Marvin Helstein <m_helstein@hotmail.com>
2024-12-08 19:38:50 +0200

    Add temporary api to pull last weeks data

---
+++ server/api/weekly/previous.get.ts
@@ -0,0 +1,22 @@
+import { getEventInfo } from "~~/server/utils/brazen-api/getEventInfo";
+import type { EventInfo } from "~~/server/utils/eventInfo";
+import { previousEventInfoFromDto, writeToDB } from "~~/server/utils/eventInfo";
+
+export default eventHandler(
+  async (event): Promise<{ event: EventInfo | null }> => {
+    const config = useRuntimeConfig(event);
+
+    console.log("Getting last weeks event from brazen api...");
+
+    const rawData = await getEventInfo(config.bzToken);
+
+    const data = previousEventInfoFromDto(rawData);
+    if (data === null) {
+      console.warn("No previous event found from brazen api!");
+    } else {
+      await writeToDB(rawData, data);
+    }
+
+    return { event: data };
+  }
+);
