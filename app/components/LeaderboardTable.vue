e84101ea655119a0337d6ca5a60efda77df20e64
Marvin Helstein <m_helstein@hotmail.com>
2024-12-09 00:49:48 +0200

    Show user icons in leaderboard

--- app/components/LeaderboardTable.vue
+++ app/components/LeaderboardTable.vue
@@ -1,9 +1,19 @@
 <template>
   <DataTable :value="entries">
     <Column field="place" header="Place" />
-    <Column field="username" header="Username">
+    <Column field="user" header="User">
       <template #body="slotProps">
-        {{ slotProps.data.user.name }}
+        <div class="flex items-center gap-2 relative">
+          <img
+            class="h-8 flex-shrink-0"
+            :src="`https://static.prod.brazenblaze.com/public-assets/profile-icon/thumbnail/${slotProps.data.user.iconId}.png`"
+          />
+          <img
+            class="h-8 absolute left-0 top-0"
+            :src="`https://static.prod.brazenblaze.com/public-assets/profile-icon-frame/thumbnail/${slotProps.data.user.iconFrameId}.png`"
+          />
+          {{ slotProps.data.user.name }}
+        </div>
       </template>
     </Column>
     <Column field="time" header="Time">
