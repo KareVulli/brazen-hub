c5db48172f42684069352672fae0c550cea1db1f
Marvin Helstein <m_helstein@hotmail.com>
2025-02-12 07:56:47 +0200

    Update game version

--- server/utils/brazen-api/client.ts
+++ server/utils/brazen-api/client.ts
@@ -6,8 +6,8 @@ export const brazenApiClient = $fetch.create({
     "Content-Type": "application/json",
     Accept: "application/json",
     "Asset-Version":
-      "2025012901/steam-pc/f222621e545e6f22a27a49b12297e3b2c107b011",
-    "Client-Version": "1.12.0",
+      "2025021200/steam-pc/81f4ddd7426d18ef61a4715672a40f468aaef65e",
+    "Client-Version": "1.13.0",
     Platform: "steam_pc",
   },
 });
