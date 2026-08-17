333c9572d60083059dff5b2e89a44fc80509f602
Marvin Helstein <m_helstein@hotmail.com>
2026-08-17 19:47:51 +0300

    Migrations?

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   dialect: "sqlite",
   schema: "./server/db/schema",
   out: "./server/db/migrations",
+  dbCredentials: {
+    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/a56888355b2792b456d66a6ac73981705520141f25e554e4849a0a54eb82ef8b.sqlite",
+  },
 });
