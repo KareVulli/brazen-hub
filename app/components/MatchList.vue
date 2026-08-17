39e80be9dbad5d84198bd5e4b33b74e8dd761273
Marvin Helstein <m_helstein@hotmail.com>
2026-08-17 23:55:58 +0300

    Remove placeholder matches

--- app/components/MatchList.vue
+++ app/components/MatchList.vue
@@ -1,6 +1,6 @@
 <template>
   <PageTitle title="Custom Matches" />
-  <div class="grid gap-2 2xl:grid-cols-2">
+  <div v-if="matches.length" class="grid gap-2 2xl:grid-cols-2">
     <Card
       v-for="match in matches"
       :key="match.id"
@@ -67,111 +67,18 @@
         </div>
       </template>
     </Card>
-    <NuxtLink to="/matches/0">
-      <Card class="border border-green-400 hover:bg-gray-800 duration-100">
-        <template #content>
-          <div class="px-2 py-0.5">
-            <div class="flex justify-between items-center">
-              <p class="font-semibold">
-                <span class="text-green-500 font-semibold">LIVE!</span> Match
-                #421 @ Wednesday, July 1, 2026 at 6:10 AM
-              </p>
-              <p>2 minutes 10 seconds</p>
-            </div>
-            <p>Round Team Match BO5 | New York City (Day)</p>
-          </div>
-          <hr class="border-t border-gray-400 mx-2 my-2" />
-          <div class="space-y-0.5">
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>CareFully, LordDeath115, Pufin</p>
-              <p class="text-green-500">1</p>
-            </div>
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>Cheeseman, Shroom, Omni</p>
-              <p class="text-red-500">0</p>
-            </div>
-          </div>
-        </template>
-      </Card>
-    </NuxtLink>
-    <NuxtLink to="/matches/1">
-      <Card class="border border-slate-800 hover:bg-gray-800 duration-100">
-        <template #content>
-          <div class="px-2 py-0.5">
-            <div class="flex justify-between items-center">
-              <p class="font-semibold">
-                Match #420 @ Wednesday, July 1, 2026 at 6:00 AM
-              </p>
-              <p>2 minutes 10 seconds</p>
-            </div>
-
-            <p>Round Team Match BO5 | New York City (Day)</p>
-          </div>
-          <hr class="border-t border-gray-400 mx-2 my-2" />
-          <div class="space-y-0.5">
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>CareFully, LordDeath115, Pufin</p>
-              <p class="text-red-500">1</p>
-            </div>
-            <div
-              class="flex justify-between font-bold bg-green-500/20 rounded-md px-2 py-0.5"
-            >
-              <p>Cheeseman, Shroom, Omni</p>
-              <p class="text-green-500">3</p>
-            </div>
-          </div>
-        </template>
-      </Card>
-    </NuxtLink>
-    <NuxtLink to="/matches/2">
-      <Card class="border border-slate-800 hover:bg-gray-800 duration-100">
-        <template #content>
-          <div class="px-2 py-0.5">
-            <div class="flex justify-between items-center">
-              <p class="font-semibold">
-                Match #419 @ Wednesday, June 25, 2026 at 6:32 PM
-              </p>
-              <p>10 minutes 2 seconds</p>
-            </div>
-            <p>Survival | New York City (Day)</p>
-            <p>
-              Winner:
-              <span class="font-semibold">CareFully</span>
-            </p>
-          </div>
-          <hr class="border-t border-gray-400 mx-2 my-2" />
-          <div class="grid grid-cols-3 gap-0.5">
-            <div
-              class="flex justify-between font-bold rounded-md px-2 py-0.5 bg-green-500/20"
-            >
-              <p>CareFully</p>
-              <p class="text-green-500">#1</p>
-            </div>
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>LordDeath115</p>
-              <p class="text-red-100">#2</p>
-            </div>
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>Pufin</p>
-              <p class="text-red-200">#3</p>
-            </div>
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>Cheeseman</p>
-              <p class="text-red-300">#4</p>
-            </div>
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>Shroom</p>
-              <p class="text-red-400">#5</p>
-            </div>
-            <div class="flex justify-between font-bold rounded-md px-2 py-0.5">
-              <p>Omni</p>
-              <p class="text-red-500">#6</p>
-            </div>
-          </div>
-        </template>
-      </Card>
-    </NuxtLink>
   </div>
+  <Card v-else>
+    <template #content>
+      <div class="px-2 py-0.5">
+        <p class="font-semibold mb-4">No custom matches yet</p>
+        <p>
+          <AppLink to="steam://launch/2511050">Invite a watcher</AppLink> to
+          your custom match to record custom matches.
+        </p>
+      </div>
+    </template>
+  </Card>
 </template>
 
 <script setup lang="ts">
@@ -183,10 +90,6 @@ const formatDuration = useFormatDuration();
 function isWinnerTeam(match: MatchDto, team: number) {
   const winnerTeam = [...match.teams].sort((a, b) => b.wins - a.wins)[0];
   return winnerTeam?.team === team;
-}
-
-function maxPlayersPerTeam(match: MatchDto) {
-  return Math.max(...match.teams.map((team) => team.teamUsers.length));
 }
 
 function matchDuration(match: MatchDto) {
