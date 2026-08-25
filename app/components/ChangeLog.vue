86e2ded26ea942af35535abde96db18584802470
Marvin Helstein <m_helstein@hotmail.com>
2026-08-25 23:18:08 +0300

    Fix changelog not filling up full width

--- app/components/ChangeLog.vue
+++ app/components/ChangeLog.vue
@@ -4,7 +4,7 @@
     :pt="{ contentWrapper: { class: 'min-w-0' } }"
   >
     <div
-      className="prose dark:prose-invert max-w-none prose-sm prose-headings:mt-0 prose-code:before:hidden prose-code:after:hidden"
+      className="prose prose-sm dark:prose-invert max-w-none prose-headings:mt-0 prose-code:before:hidden prose-code:after:hidden"
     >
       <ContentRenderer v-if="data" :value="data" :components="headings" />
     </div>
